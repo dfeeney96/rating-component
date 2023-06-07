0616f08161b81b8c599a968585deadce50744def
dfeeney96 <daniellefeeney96@gmail.com>
2023-06-07 20:48:14 +0100

    add error card and button interaction functionality

--- script.js
+++ script.js
@@ -5,12 +5,29 @@ const four = document.querySelector(".four");
 const five = document.querySelector(".five");
 
 let scoreButtons = [one, two, three, four, five];
+let activeButtons = [];
 
 let chosenScore;
 
 for(let i = 0; i < scoreButtons.length; i++){
     scoreButtons[i].addEventListener("click", function(){
-        chosenScore = this.innerText;
+        for(let j = 0; j < scoreButtons.length; j++){
+            if(scoreButtons[j].getAttribute("id") === "selected" && this !== scoreButtons[j]){
+                scoreButtons[j].removeAttribute("id", "selected");
+                scoreButtons[j].previousElementSibling.removeAttribute("id", "prior-score");
+            }
+        }
+
+        if(this.getAttribute("id") !== "selected"){
+            if(this.innerText !=="1"){
+                this.previousElementSibling.setAttribute("id", "prior-score");
+            }
+            this.setAttribute("id", "selected");
+            chosenScore = this.innerText;
+        } else {
+            this.removeAttribute("id", "selected");
+            chosenScore = "";
+        }
     }
     )
 }
@@ -18,12 +35,25 @@ for(let i = 0; i < scoreButtons.length; i++){
 
 const submit = document.querySelector(".submit");
 const ratingCard = document.querySelector(".rating");
+const errorCard = document.querySelector(".error");
 const thankyouCard = document.querySelector(".thankyou");
 const confirmationMessage = document.querySelector(".thankyou h2")
 
 submit.addEventListener("click", function(){
-    ratingCard.classList.add("hide-card");
-    thankyouCard.classList.add("submission-received");
-    confirmationMessage.innerHTML = `You selected ${chosenScore} out of 5`;
+    if(chosenScore){
+        ratingCard.classList.add("hide-card");
+        thankyouCard.classList.add("submission-received");
+        confirmationMessage.innerHTML = `You selected ${chosenScore} out of 5`;
+    } else {
+        ratingCard.classList.add("hide-card");
+        errorCard.classList.add("no-score")
+    }
 
+})
+
+const returnButton = document.querySelector(".return");
+
+returnButton.addEventListener("click", function(){
+    errorCard.classList.remove("no-score")
+    ratingCard.classList.remove("hide-card");
 })
